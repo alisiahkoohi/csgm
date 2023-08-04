110477fffd21ef998929bd7e229b091adffa8c8e
Ali Siahkoohi <ali.siahkoohi@gmail.com>
2023-08-03 20:19:45 -0500

    runned experiments

---
+++ run_imaging.sh
@@ -0,0 +1,4 @@
+python scripts/train_conditional_seismic_imaging.py --dataset seismic --modes 24 --cuda 0 &
+python scripts/train_conditional_seismic_imaging.py --dataset seismic --modes 12 --cuda 1 &
+python scripts/train_conditional_seismic_imaging.py --dataset seismic --modes 6  --cuda 2 &
+
